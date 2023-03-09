d81691ab9c9cbf76f3ab70f1c569b1f5a554fe9e
Marion <mrobert148@gmail.com>
2023-03-09 13:15:49 +0100

    create a graph in pixela

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests
 import os
 
-PIXE_API_ENDPOINT = "https://pixe.la/v1/users"
-PIXE_API_PARAMS = {
-    "token": os.environ["PIXE_API_TOKEN"],
-    "username": os.environ["PIXE_API_USERNAME"],
+
+PIXELA_API_ENDPOINT = "https://pixe.la/v1/users"
+token = os.environ["PIXELA_API_TOKEN"]
+username = os.environ["PIXELA_API_USERNAME"]
+PIXELA_API_PARAMS = {
+    "token": token,
+    "username": username,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-
-# response = requests.post(url=PIXE_API_ENDPOINT, json=PIXE_API_PARAMS)
+# create account on pixela
+# response = requests.post(url=PIXELA_API_ENDPOINT, json=PIXELA_API_PARAMS)
 # print(response.text)
+
+GRAPH_ENDPOINT = f"{PIXELA_API_ENDPOINT}/{username}/graphs"
+GRAPH_PARAMS = {
+    "id": "graph1",
+    "name": "Meditation Time Graph",
+    "unit": "minutes",
+    "type": "int",
+    "color": "sora",
+}
+GRAPH_HEADERS = {
+    "X-USER-TOKEN": token
+}
+
+# create a graph
+response = requests.post(url=GRAPH_ENDPOINT, json=GRAPH_PARAMS, headers=GRAPH_HEADERS)
+print(response.text)
